a8012c0f741ceb2cdbec74899c6b606a77e1f8b9
Tiksten <tiksten@outlook.com>
2021-02-16 13:03:20 +0300

    Fix

--- Lesson5/Lesson5/Lesson5/Properties/AssemblyInfo.cs
+++ Lesson5/Lesson5/Lesson5/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lesson4")]
+[assembly: AssemblyTitle("Lesson5")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lesson4")]
+[assembly: AssemblyProduct("Lesson5")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("db26303b-5c5c-429a-ae5a-37c62a7155f3")]
+[assembly: Guid("f8c5727c-136c-4acb-9e89-399c1e925ee9")]
 
 // Version information for an assembly consists of the following four values:
 //
